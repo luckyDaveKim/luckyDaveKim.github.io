b27022e36d7ed5f835fe910bfe4fcbc1fbc7a592
Dave <lucky_dave@naver.com>
2020-10-18 13:33:47 +0900

    feat: Add Google search console

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-109095118-1',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
