706dccf5cf766eb0a618954ef5cc79ead44cf87d
Dave <lucky_dave@naver.com>
2020-10-22 03:26:35 +0900

    fix: Fix pagination prev link
    - resolve #10

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import _ from 'lodash';
+import path from 'path'
 import { setLightness } from 'polished';
 import { css } from '@emotion/core';
 
@@ -16,9 +17,8 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({ currentPage, num
   const isFirst = currentPage === 1;
   const isLast = currentPage === numPages;
   // remove single slash for avoid duplicated slash
-  pathPrefix = (pathPrefix === '/' ? '' : pathPrefix);
-  const prevPage = currentPage === 2 ? pathPrefix : `${pathPrefix}/${currentPage - 1}`;
-  const nextPage = `${pathPrefix}/${currentPage + 1}`;
+  const prevPage = currentPage === 2 ? pathPrefix : path.join(pathPrefix, `${currentPage - 1}`);
+  const nextPage = path.join(pathPrefix, `${currentPage + 1}`);
   const pagesPerBlock = 5;
   const currentBlock = Math.ceil(currentPage / pagesPerBlock);
   const startPageIndex = (currentBlock - 1) * pagesPerBlock;
@@ -38,7 +38,7 @@ const Pagination: React.FunctionComponent<PaginationProps> = ({ currentPage, num
 
             {_.range(startPageIndex, endPageIndex)
               .map(i => (
-                <Link key={`pagination-number${i + 1}`} className={i + 1 === currentPage ? 'active' : ''} to={`${pathPrefix}/${i === 0 ? '' : i + 1}`}>
+                <Link key={`pagination-number${i + 1}`} className={i + 1 === currentPage ? 'active' : ''} to={path.join(pathPrefix, `${i === 0 ? '' : i + 1}`)}>
                   {i + 1}
                 </Link>
               ))}
