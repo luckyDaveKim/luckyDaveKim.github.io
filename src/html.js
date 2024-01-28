51611e34c3cf0ed41901b422620412c1698b3e8c
mingyu.k <mingyu.k@navercorp.com>
2024-01-28 10:29:09 +0900

    feat: Add googleAnalytics

--- src/html.js
+++ src/html.js
@@ -11,6 +11,18 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3GKM3N4DZE"></script>
+        <script>
+          {
+            `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3GKM3N4DZE');
+            `
+          }
+        </script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
